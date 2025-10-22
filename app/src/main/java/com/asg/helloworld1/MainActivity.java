2038665a434264d5f8b756037b88c9f5d717085e
Mortir87 <aitor@blanco.casa>
2025-10-22 21:40:38 +0200

    Add Conexion MySql y Lectura 1 linea de tabla

--- app/src/main/java/com/asg/helloworld1/MainActivity.java
+++ app/src/main/java/com/asg/helloworld1/MainActivity.java
@@ -1,24 +1,63 @@
 package com.asg.helloworld1;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.util.Log;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        connectarConMySQL();
+    }
+
+    private void connectarConMySQL() {
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Connection conex = ConexBBDD.getConnection();
+
+                    if (conex != null) {
+                        Log.d(TAG, "¡Conexión a la base de datos exitosa!");
+
+                        Statement statement = conex.createStatement();
+                        ResultSet resultSet = statement.executeQuery("SELECT nombre, apellido FROM alumnos");
+
+                        // Usamos un if para asegurarnos de que hay al menos un resultado
+                        if (resultSet.next()) {
+                            String nombre = resultSet.getString("nombre");
+                            String apellido = resultSet.getString("apellido");
+                            Log.d(TAG, "Alumno: " + nombre + " " + apellido);
+                        } else {
+                            Log.d(TAG, "No se encontraron registros en la tabla 'alumnos'.");
+                        }
+
+                        //Cerrar los recursos
+                        resultSet.close();
+                        statement.close();
+                        conex.close();
+
+                    } else {
+                        Log.e(TAG, "Fallo al conectar a la base de datos.");
+                    }
+
+                } catch (SQLException e) {
+                    Log.e(TAG, "Error de SQL: " + e.getMessage());
+                    e.printStackTrace();
+                }
+            }
         });
+        thread.start();
     }
 }
