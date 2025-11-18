dfe706a9f104a8b7e49bb301f534cf6b128c549b
Mortir87 <aitor@blanco.casa>
2025-11-18 22:42:48 +0100

    Se agrega fondo, clienteActivity y activitycliente, se conexion en ventana secuntaria

---
+++ app/src/main/java/com/asg/helloworld1/ClienteActivity.java
@@ -0,0 +1,101 @@
+package com.asg.helloworld1;
+
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class ClienteActivity extends AppCompatActivity {
+
+    private static final String TAG = "ClienteActivity";
+
+    private EditText etNombre;
+    private EditText etApellido;
+    private Button btnAgregar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cliente);
+
+        // Enlazamos los componentes.
+        etNombre = findViewById(R.id.editext_nombre);
+        etApellido = findViewById(R.id.editext_apellido);
+        btnAgregar = findViewById(R.id.botonAgregar);
+
+        // actuador onClick
+        btnAgregar.setOnClickListener(v -> {
+            // Obtenemos el texto
+            String nombre = etNombre.getText().toString().trim();
+            String apellido = etApellido.getText().toString().trim();
+
+            // utilizamos isEmpty para que no este vacio
+            if (TextUtils.isEmpty(nombre)) {
+                etNombre.setError("Este campo es obligatorio");
+                return; // Detenemos ejecucion.
+            }
+
+            if (TextUtils.isEmpty(apellido)) {
+                etApellido.setError("Este campo es obligatorio");
+                return; // Detenemos ejecucion.
+            }
+
+            // llamamos a nuestra funcion insertar
+            insertarCliente(nombre, apellido);
+
+            // limpiamos
+            etNombre.setText("");
+            etApellido.setText("");
+            
+        });
+    }
+
+    //Creamos la funcion para insertar
+    private void insertarCliente(String nombre, String apellido) {
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Connection conex = ConexBBDD.getConnection();
+
+                    if (conex != null) {
+                        Log.d(TAG, "¡Conexión a la base de datos exitosa!");
+                        //Toast.makeText(ClienteActivity.this, "¡Conexión a la base de datos exitosa!", Toast.LENGTH_SHORT).show();
+
+
+                        // usamos PreparedStatement para seguridad y eficiencia
+                        String sqlInsert = "INSERT INTO CLIENTE (nombre, apellido) VALUES (?, ?)";
+                        PreparedStatement prepStatement = conex.prepareStatement(sqlInsert);
+                        prepStatement.setString(1, nombre);
+                        prepStatement.setString(2, apellido);
+
+                        // comprobamos si se ha actualizado
+                        int fAfectadas = prepStatement.executeUpdate();
+                        Log.d(TAG, "INSERT correcto: " + fAfectadas);
+
+                        // cerramos los recursos
+                        prepStatement.close();
+                        conex.close();
+
+                    } else {
+                        Log.e(TAG, "Fallo al conectar a la base de datos.");
+                        //Toast.makeText(ClienteActivity.this, "Fallo al conectar la BBDD", Toast.LENGTH_SHORT).show();
+                    }
+
+                } catch (SQLException e) {
+                    Log.e(TAG, "Error de SQL: " + e.getMessage());
+                    e.printStackTrace();
+                }
+            }
+        });
+        thread.start();
+    }
+}
--- app/src/main/java/com/asg/helloworld1/MainActivity.java
+++ app/src/main/java/com/asg/helloworld1/MainActivity.java
@@ -1,77 +1,22 @@
 package com.asg.helloworld1;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.view.View;
-import android.widget.Toast;
-
 import com.google.android.material.button.MaterialButton;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-
 public class MainActivity extends AppCompatActivity {
-
-    private static final String TAG = "MainActivity";
-    MaterialButton ejemplBotton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        connectarConMySQL();;
+        MaterialButton btn_Cliente = findViewById(R.id.BotonCliente);
 
-    }
-
-    private void connectarConMySQL() {
-        Thread thread = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    Connection conex = ConexBBDD1.getConnection();
-
-                    if (conex != null) {
-                        Log.d(TAG, "¡Conexión a la base de datos exitosa!");
-
-                        Statement statement = conex.createStatement();
-
-                        //Probamos escritura en bbdd
-                        String insertPrueba = "INSERT INTO alumnos (nombre, apellido) VALUES ('Juan', 'Perez')";
-
-                        int celdasAfectadas = statement.executeUpdate(insertPrueba);
-                        Log.d(TAG, "INSERT correcto. Filas afectadas: " + celdasAfectadas); // != 1 es error.
-
-                        //Probamos lectura
-                        ResultSet resultSet = statement.executeQuery("SELECT nombre, apellido FROM alumnos");
-
-                        if (resultSet.next()) {
-                            String nombre = resultSet.getString("nombre");
-                            String apellido = resultSet.getString("apellido");
-                            Log.d(TAG, "Lectura correcta - Alumno: " + nombre + " " + apellido);
-                        } else {
-                            Log.d(TAG, "No se encontraron registros en la tabla 'alumnos'.");
-                        }
-
-                        //CERRAR RECURSOS
-                        resultSet.close();
-                        statement.close();
-                        conex.close();
-
-                    } else {
-                        Log.e(TAG, "Fallo al conectar a la base de datos.");
-                    }
-
-                } catch (SQLException e) {
-                    Log.e(TAG, "Error de SQL: " + e.getMessage());
-                    e.printStackTrace();
-                }
-            }
+        btn_Cliente.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, ClienteActivity.class);
+            startActivity(intent);
         });
-        thread.start();
     }
 }
